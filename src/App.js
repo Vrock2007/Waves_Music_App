0ea96cba4caa7578a6494742f99ce122542a12cf
Vishal Choudhary <vishal200712@gmail.com>
2021-05-27 12:19:21 +0530

    implemented play/pause

--- src/App.js
+++ src/App.js
@@ -11,12 +11,16 @@ function App() {
   //State
   const [songs, setSongs] = useState(data());
   const [currentSong, setCurrentSong] = useState(songs[0]); 
+  const [isPlaying, setIsPlaying] = useState(false);
 
   return (
     <div className="App">
-      <Song currentSong={currentSong}/>
-      <Player />
-     
+      <Song currentSong={currentSong} />
+      <Player 
+        setIsPlaying={setIsPlaying} 
+        isPlaying={isPlaying} 
+        currentSong={currentSong} 
+      />
     </div>
   );
 }
