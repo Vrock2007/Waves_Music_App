991da32398d01ba1a6a0dd66b9ee50b50c66e036
Vishal Choudhary <vishal200712@gmail.com>
2021-05-27 20:50:55 +0530

    Added seek functionality

--- src/components/Player.js
+++ src/components/Player.js
@@ -28,6 +28,11 @@ const Player = ({currentSong, isPlaying, setIsPlaying}) => {
       )
     }
 
+    const dragHandler = (e) => {
+      audioRef.current.currentTime = e.target.value;
+      setSongInfo({...songInfo, currentTime: e.target.value});
+    }
+
     //State
     const [songInfo, setSongInfo] = useState({
       currentTime: null,
@@ -39,8 +44,14 @@ const Player = ({currentSong, isPlaying, setIsPlaying}) => {
       <div className="player">
         <div className="time-control">
           <p>{getTime(songInfo.currentTime)}</p>
-          <input type="range" />
-          <p>{getTime(songInfo.duration)}</p>
+          <input 
+            min={0} 
+            max={songInfo.duration} 
+            value={songInfo.currentTime} 
+            onChange={dragHandler}
+            type="range" 
+          />
+          <p>{getTime(songInfo.duration)}</p> 
         </div>
         <div className="play-control">
           <FontAwesomeIcon className="skip-back" size="2x" icon={faAngleLeft} />
