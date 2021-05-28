d13014f5bae831f9901af234d5ccbe7cb8c4eb50
Archit Mishra <archit.mishra21@outlook.com>
2021-05-28 11:31:37 +0530

    more updates to library

--- src/components/LibrarySong.js
+++ src/components/LibrarySong.js
@@ -3,9 +3,11 @@ import React from 'react'
 const LibrarySong = ({song}) => {
     return (
       <div className="library-songs">
-        <img src={song.cover}></img>
+        <img alt={song.name} src={song.cover}></img>
+        <div className="song-description">
         <h3>{song.name}</h3>
         <h4>{song.artist}</h4>
+        </div>
       </div>
     );
 }
