2c1e7596c97c827e1c31167deda16c00f8a5a044
Archit Mishra <archit.mishra21@outlook.com>
2021-05-31 14:06:29 +0530

    customizing seek

--- src/components/Player.js
+++ src/components/Player.js
@@ -85,13 +85,16 @@ const Player = ({
       <div className="player">
         <div className="time-control">
           <p>{getTime(songInfo.currentTime)}</p>
-          <input
-            min={0}
-            max={songInfo.duration || 0}
-            value={songInfo.currentTime}
-            onChange={dragHandler}
-            type="range"
-          />
+          <div className="track">
+            <input
+              min={0}
+              max={songInfo.duration || 0}
+              value={songInfo.currentTime}
+              onChange={dragHandler}
+              type="range"
+            />
+            <div className="animate-track"></div>
+          </div>
           <p>{songInfo.duration ? getTime(songInfo.duration) : "0:00"}</p>
         </div>
         <div className="play-control">
