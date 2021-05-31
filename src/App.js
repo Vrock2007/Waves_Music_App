e190461a0133d35f9b351abe0ec2acc34db78c8f
Vishal Choudhary <vishal200712@gmail.com>
2021-05-31 16:48:38 +0530

    Customizing input

--- src/App.js
+++ src/App.js
@@ -22,6 +22,7 @@ function App() {
   const [songInfo, setSongInfo] = useState({
     currentTime: 0,
     duration: 0,
+    animationPercentage: 0,
   });
   //Toggle Library
   const [libraryStatus, setLibraryStatus] = useState(false);
@@ -29,7 +30,17 @@ function App() {
   const timeUpdateHandler = (e) => {
     const current = e.target.currentTime;
     const duration = e.target.duration;
-    setSongInfo({ ...songInfo, currentTime: current, duration: duration });
+
+    //Calculate Percentage
+    const roundedCurrent = Math.round(current);
+    const roundedDuration = Math.round(duration);
+    const animation = Math.round((roundedCurrent / roundedDuration) * 100);
+    
+    setSongInfo({ ...songInfo, 
+      currentTime: current, 
+      duration: duration,  
+      animationPercentage: animation,
+    });
   };
 
   return (
--- src/components/Player.js
+++ src/components/Player.js
@@ -80,12 +80,21 @@ const Player = ({
       playAudio(isPlaying, audioRef);
     };
     
+    //Adding the Styles
+    const trackAnim = {
+      transform: `translateX(${songInfo.animationPercentage}%)`
+    } 
 
     return (
       <div className="player">
         <div className="time-control">
           <p>{getTime(songInfo.currentTime)}</p>
-          <div className="track">
+          <div
+            style={{
+              background: `linear-gradient(to right, ${currentSong.color[0]}, ${currentSong.color[1]})`,
+            }}
+            className="track"
+          >
             <input
               min={0}
               max={songInfo.duration || 0}
@@ -93,7 +102,7 @@ const Player = ({
               onChange={dragHandler}
               type="range"
             />
-            <div className="animate-track"></div>
+            <div style={trackAnim} className="animate-track"></div>
           </div>
           <p>{songInfo.duration ? getTime(songInfo.duration) : "0:00"}</p>
         </div>
